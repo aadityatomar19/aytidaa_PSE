bef12bb4c734ea97e1e89e47ac53a860f02cf1c5
aaditya tomar <aadityatomar19@gmail.com>
2022-07-25 16:45:16 +0530

    Added arduino codebase for SPS30

---
+++ SPS30/Arduino_IDE/sps30_esp32/sps30_esp32.ino
@@ -0,0 +1,232 @@
+/************************************************************************************
+ *
+ *  =========================  Hardware connections =================================
+ *  /////////////////////////////////////////////////////////////////////////////////
+ *  ## UART UART UART UART UART UART UART UART UART UART UART UART UART UART UART  ##
+ *  /////////////////////////////////////////////////////////////////////////////////
+ *
+ *  Sucessfully test has been performed on an ESP32:
+ *
+ *  Using Serial1, setting the RX-pin(25) and TX-pin(26)
+ *
+ *  SPS30 pin     ESP32
+ *  1 VCC -------- VUSB
+ *  2 RX  -------- TX  pin 26 !! NEEDS A CHANGE ON LINE 130
+ *  3 TX  -------- RX  pin 25 !! NEEDS A CHANGE ON LINE 130
+ *  4 Select      (NOT CONNECTED)
+ *  5 GND -------- GND
+ */
+#include "sps30.h"
+#define SP30_COMMS Serial2
+
+/////////////////////////////////////////////////////////////
+/* define driver debug
+ * 0 : no messages
+ * 1 : request sending and receiving
+ * 2 : request sending and receiving + show protocol errors */
+ //////////////////////////////////////////////////////////////
+#define DEBUG 2
+// function prototypes (sometimes the pre-processor does not create prototypes themself on ESPxx)
+void serialTrigger(char * mess);
+void ErrtoMess(char *mess, uint8_t r);
+void Errorloop(char *mess, uint8_t r);
+void GetDeviceInfo();
+bool read_all();
+
+// create constructor
+SPS30 sps30;
+
+void setup() {
+
+  Serial.begin(115200);
+  // set driver debug level
+  sps30.EnableDebugging(DEBUG);
+
+  Serial.println(F("Trying to connect."));
+  SP30_COMMS.begin(115200, SERIAL_8N1, 25, 26);
+  // Initialize SPS30 library
+  if (! sps30.begin(&SP30_COMMS))
+    Errorloop((char *) "Could not set serial communication channel.", 0);
+
+  // check for SPS30 connection
+  if (! sps30.probe()) Errorloop((char *) "could not probe / connect with SPS30.", 0);
+  else  Serial.println(F("Detected SPS30."));
+
+  // reset SPS30 connection
+  if (! sps30.reset()) Errorloop((char *) "could not reset.", 0);
+
+  // read device info
+//  GetDeviceInfo();
+
+  // start measurement
+  if (sps30.start()) Serial.println(F("Measurement started"));
+  else Errorloop((char *) "Could NOT start measurement", 0);
+
+//  serialTrigger((char *) "Hit <enter> to continue reading.");
+}
+
+void loop() {
+  read_all();
+  delay(3000);
+}
+
+/**
+ * @brief : read and display device info
+ */
+void GetDeviceInfo()
+{
+  char buf[32];
+  uint8_t ret;
+  SPS30_version v;
+
+  //try to read serial number
+  ret = sps30.GetSerialNumber(buf, 32);
+  if (ret == SPS30_ERR_OK) {
+    Serial.print(F("Serial number : "));
+    if(strlen(buf) > 0)  Serial.println(buf);
+    else Serial.println(F("not available"));
+  }
+  else
+    ErrtoMess((char *) "could not get serial number", ret);
+
+  // try to get product name
+  ret = sps30.GetProductName(buf, 32);
+  if (ret == SPS30_ERR_OK)  {
+    Serial.print(F("Product name  : "));
+
+    if(strlen(buf) > 0)  Serial.println(buf);
+    else Serial.println(F("not available"));
+  }
+  else
+    ErrtoMess((char *) "could not get product name.", ret);
+
+  // try to get version info
+  ret = sps30.GetVersion(&v);
+  if (ret != SPS30_ERR_OK) {
+    Serial.println(F("Can not read version info"));
+    return;
+  }
+
+  Serial.print(F("Firmware level: "));  Serial.print(v.major);
+  Serial.print(".");  Serial.println(v.minor);
+
+  Serial.print(F("Hardware level: "));  Serial.println(v.HW_version);
+
+  Serial.print(F("SHDLC protocol: ")); Serial.print(v.SHDLC_major);
+  Serial.print(".");  Serial.println(v.SHDLC_minor);
+
+  Serial.print(F("Library level : "));  Serial.print(v.DRV_major);
+  Serial.print(".");  Serial.println(v.DRV_minor);
+}
+
+/**
+ * @brief : read and display all values
+ */
+bool read_all()
+{
+  static bool header = true;
+  uint8_t ret, error_cnt = 0;
+  struct sps_values val;
+
+  // loop to get data
+  do {
+
+    ret = sps30.GetValues(&val);
+
+    // data might not have been ready
+    if (ret == SPS30_ERR_DATALENGTH){
+
+        if (error_cnt++ > 3) {
+          ErrtoMess((char *) "Error during reading values: ",ret);
+          return(false);
+        }
+        delay(1000);
+    }
+
+    // if other error
+    else if(ret != SPS30_ERR_OK) {
+      ErrtoMess((char *) "Error during reading values: ",ret);
+      return(false);
+    }
+
+  } while (ret != SPS30_ERR_OK);
+
+  // only print header first time
+  if (header) {
+    Serial.println(F("-------------Mass -----------    ------------- Number --------------   -Average-"));
+    Serial.println(F("     Concentration [μg/m3]             Concentration [#/cm3]             [μm]"));
+    Serial.println(F("P1.0\tP2.5\tP4.0\tP10\tP0.5\tP1.0\tP2.5\tP4.0\tP10\tPartSize\n"));
+    header = false;
+  }
+
+  Serial.print(val.MassPM1);
+  Serial.print(F("\t"));
+  Serial.print(val.MassPM2);
+  Serial.print(F("\t"));
+  Serial.print(val.MassPM4);
+  Serial.print(F("\t"));
+  Serial.print(val.MassPM10);
+  Serial.print(F("\t"));
+  Serial.print(val.NumPM0);
+  Serial.print(F("\t"));
+  Serial.print(val.NumPM1);
+  Serial.print(F("\t"));
+  Serial.print(val.NumPM2);
+  Serial.print(F("\t"));
+  Serial.print(val.NumPM4);
+  Serial.print(F("\t"));
+  Serial.print(val.NumPM10);
+  Serial.print(F("\t"));
+  Serial.print(val.PartSize);
+  Serial.print(F("\n"));
+
+  return(true);
+}
+
+/**
+ *  @brief : continued loop after fatal error
+ *  @param mess : message to display
+ *  @param r : error code
+ *
+ *  if r is zero, it will only display the message
+ */
+void Errorloop(char *mess, uint8_t r)
+{
+  if (r) ErrtoMess(mess, r);
+  else Serial.println(mess);
+  Serial.println(F("Program on hold"));
+  for(;;) delay(100000);
+}
+
+/**
+ *  @brief : display error message
+ *  @param mess : message to display
+ *  @param r : error code
+ *
+ */
+void ErrtoMess(char *mess, uint8_t r)
+{
+  char buf[80];
+
+  Serial.print(mess);
+
+  sps30.GetErrDescription(r, buf, 80);
+  Serial.println(buf);
+}
+
+/**
+ * serialTrigger prints repeated message, then waits for enter
+ * to come in from the serial port.
+ */
+void serialTrigger(char * mess)
+{
+  Serial.println();
+
+  while (!Serial.available()) {
+    Serial.println(mess);
+    delay(2000);
+  }
+
+  while (Serial.available())
+    Serial.read();
+}
